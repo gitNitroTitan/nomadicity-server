84be50aaa90be25307608deaa2aa5bfd5a91c3d6
Jeremiah King <usaxray25@gmail.com>
2023-01-21 16:23:16 -0600

    user not working when authenticated

--- nomadicityapi/models/boards.py
+++ nomadicityapi/models/boards.py
@@ -3,11 +3,9 @@ from django.db import models
 
 class Board(models.Model):
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     image_url = models.URLField(max_length=200)
     description = models.CharField(max_length=1000)
-    # objects = models.Manager()
 
     def __str__(self):
         return self.name
